cf6d0385502754bec3c93c0174426e05fcd66b1a
abishek baskaran <abisheksnowflake@gmail.com>
2025-09-09 04:39:50 +0000

    added date macros

---
+++ macros/date_utils.sql
@@ -0,0 +1,7 @@
+{% macro get_season(x) %}
+CASE WHEN MONTH(TO_TIMESTAMP({{x}})) IN (12, 1, 2) THEN 'winter' WHEN MONTH(TO_TIMESTAMP({{x}})) IN (3, 4, 5) THEN 'spring' WHEN MONTH(TO_TIMESTAMP({{x}})) IN (6, 7, 8) THEN 'summer' ELSE 'autumn' END
+{% endmacro %}
+
+{% macro get_daytype(x) %}
+    CASE WHEN DAYNAME(TO_TIMESTAMP({{x}})) IN ('Sat', 'Sun') THEN 'weekend' ELSE 'businessday' END
+{% endmacro %}
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,8 +3,8 @@ WITH cte AS (
         TO_TIMESTAMP(started_at) AS started_at,
         DATE(TO_TIMESTAMP(started_at)) AS date_started_at,
         HOUR(TO_TIMESTAMP(started_at)) AS hour_started_at,
-        CASE WHEN DAYNAME(TO_TIMESTAMP(started_at)) IN ('Sat', 'Sun') THEN 'weekend' ELSE 'businessday' END AS day_type,
-        CASE WHEN MONTH(TO_TIMESTAMP(started_at)) IN (12, 1, 2) THEN 'winter' WHEN MONTH(TO_TIMESTAMP(started_at)) IN (3, 4, 5) THEN 'spring' WHEN MONTH(TO_TIMESTAMP(started_at)) IN (6, 7, 8) THEN 'summer' ELSE 'autumn' END AS season  
+        {{get_daytype('STARTED_AT')}} AS day_type,
+        {{get_season('STARTED_AT')}} AS season 
      FROM {{ source('demo', 'bike') }}
     WHERE started_at != 'started_at'
 )
